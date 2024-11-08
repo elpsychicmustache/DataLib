48fed00e31a0da177696bf93d906f56c4e7f12b7
CodyD <codeproductions36@gmail.com>
2024-11-07 19:05:15 -0700

    Updated style - got rid of a bunch of extra paragraph spaces

--- src/dataframe_manager.py
+++ src/dataframe_manager.py
@@ -8,6 +8,7 @@ import pandas as pd
 from .validate_input import get_user_confirmation, validate_argument
 from .utilities import get_df_from_csv
 
+
 class DataframeManager:
 
     def __init__(self, dataframe: pd.DataFrame=None, file_path: str="../data/input/", file_name: str="data.csv", date_columns: list[str]=None, column_names: list[str]=None) -> None:
@@ -20,14 +21,11 @@ class DataframeManager:
             column_names (list[str], optional): The names to provide each column. Defaults to None.
         """
 
-
         if dataframe:
             self._dataframe = dataframe
         else:
             self._dataframe: pd.DataFrame = get_df_from_csv(file_path, file_name, date_columns, column_names)
         
-
-
     def understand_data(self, head_tail_size: int = 20, analysis_type="short") -> None:
         """Step one of Exploratory data anlysis. Prints some information to help understand the dataframe.
 
@@ -46,7 +44,7 @@ class DataframeManager:
         if analysis_type == "long":
             self._show_null_values()
 
-    
+
     # COLLECTION OF SIMPLE PRINT FUNCTIONS
     def _explain_shape(self):
         """Prints the shape of the dataframe.
@@ -76,6 +74,7 @@ class DataframeManager:
             len(self._dataframe[column].loc[self._dataframe[column].isna()])
     # END OF COLLECTION OF SIMPLE PRINT FUNCTIONS
 
+
     def prepare_data(self, ignore_remove:bool=False, ignore_rename:bool=False) -> None:
         """Step two of Exploratory Data Analysis. Provides some generic function that help with processing data.
         """
@@ -83,7 +82,6 @@ class DataframeManager:
             self.remove_columns_interactively()
         if not ignore_rename:
             self.rename_columns_interactively()
-
 
     def remove_columns_interactively(self) -> None:
         """Provides the user a way to interactively delete columns from the dataframe.
@@ -102,7 +100,6 @@ class DataframeManager:
         else:
             print("[-] No columns removed!")
 
-
     def _prompt_for_columns_to_remove(self) -> list[str]:
         """Prompts the user each column name and asks if they want to remove them.
 
@@ -120,7 +117,6 @@ class DataframeManager:
 
         return columns_to_remove
     
-
     def rename_columns_interactively(self) -> None:
         """Provides the user a way to interactively rename the columns.
         """
@@ -138,7 +134,6 @@ class DataframeManager:
         else:
             print("[-] No columns renamed!")
 
-
     def _prompt_for_columns_to_rename(self) -> dict[str, str]:
         """Prompts the user a way to interactively rename the columns of the dataframe.
 
@@ -155,7 +150,6 @@ class DataframeManager:
                 columns_to_rename[column] = new_column_name
 
         return columns_to_rename
-
 
     def __str__(self) -> str:
         return f"This is a pandas DataFrame object. Here are the first 25 rows: {self._dataframe.head(25)}"
