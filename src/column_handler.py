61c426deb3c8089e28687ffedecb92f493eadb2d
ElPsychicMustache <cody.duran@psychicmustache.com>
2024-11-24 18:39:39 -0700

    Moved remove_columns over to ColumnHandler

--- src/column_handler.py
+++ src/column_handler.py
@@ -5,15 +5,36 @@
 
 import pandas as pd
 
+from utilities import prompt_selection_for_column_list
+from validate_input import get_user_confirmation
+
 class ColumnHandler:
     def __init__(self, dataframe: pd.DataFrame) -> None:
-        self.dataframe = dataframe
+        self._dataframe = dataframe
 
     def remove_columns(self) -> None:
-        pass
+        """Provides the user a way to interactively delete columns from the dataframe.
+        """
+    
+        user_wants_to_remove_columns: bool = get_user_confirmation(message="[*] Would you like to remove any columns? [Y/n]: ", true_options=["y", "yes", ""], false_options=["n", "no"])
+
+        columns_to_remove: list[str] = []
+        if user_wants_to_remove_columns:
+            columns_to_remove = prompt_selection_for_column_list(message="[*] Please enter the numbers next to each column that you would like to remove. Leave blank to ignore.", list_of_options=self._dataframe.columns, default_all=False)
+
+        if columns_to_remove:    
+            print(f"[!] Removing the following columns: {columns_to_remove}")
+            self._dataframe = self._dataframe.drop(columns=columns_to_remove)
+            print(f"[+] Columns removed!")
+        else:
+            print("[-] No columns removed!")
 
     def rename_columns(self) -> None:
         pass
 
     def analyze_dtypes(self) -> None:
         pass
+
+    @property
+    def dataframe(self) -> pd.DataFrame:
+        return self._dataframe
