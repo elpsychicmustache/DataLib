e3c31abd6aeea93639fb9d0a977dadf3faf1637b
ElPsychicMustache <cody.duran@psychicmustache.com>
2024-11-24 18:44:22 -0700

    Moved analyze_dtypes over to column_handler

--- src/column_handler.py
+++ src/column_handler.py
@@ -5,7 +5,7 @@
 
 import pandas as pd
 
-from utilities import prompt_selection_for_column_list, prompt_for_columns_to_rename
+from utilities import prompt_selection_for_column_list, prompt_for_columns_to_rename, prompt_user_for_int
 from validate_input import get_user_confirmation
 
 class ColumnHandler:
@@ -52,7 +52,55 @@ class ColumnHandler:
             print("[-] No columns renamed!")
 
     def analyze_dtypes(self) -> None:
-        pass
+        self._explain_dtypes()
+        print()
+        if get_user_confirmation(
+            message="[*] Would you like to change any d-types? [y/N] ", 
+            true_options=["y", "yes"], 
+            false_options=["n", "no", ""]
+            ):
+
+            columns_to_update: list[str] = prompt_selection_for_column_list(message="[*] Please enter the numbers next to each column that you would like to change the d-type. Leaving blank skips this step.", list_of_options=self._dataframe.columns, default_all=False)
+            self._ask_new_dtypes(columns=columns_to_update)
+    
+    def _explain_dtypes(self):
+        """Prints the column names and dtypes.
+        """
+        print(f"======= Columns and dtypes ======= \nThe dataframe's column names and types are the following: \n{self._dataframe.dtypes}")
+    
+
+    def _ask_new_dtypes(self, columns:list[str]):
+
+        if not columns:
+            return 
+        
+        options: dict[int, str] = {
+            0: "Do nothing",
+            1: "Change to datetime",
+            2: "Change to numeric",
+            3: "Change to categorical"
+        }
+
+        print()
+        for column in columns:
+            change_to_value = prompt_user_for_int(message=f"[*] What would you like to do with column {column}?", options=options)
+            
+            if change_to_value == 0:
+                print("[!] Doing nothing.")
+                pass
+            elif change_to_value == 1:
+                self._change_column_to_datetime(column_name=column)
+            elif change_to_value == 2:
+                self._change_column_to_numeric(column_name=column)
+            elif change_to_value == 3:
+                self._change_column_to_categorical(column_name=column)
+            
+    def _change_column_to_datetime(self, column_name:str) -> None:
+        self._dataframe[column_name] = pd.to_datetime(self._dataframe[column_name])
+    def _change_column_to_numeric(self, column_name:str, numeric_type:str) -> None:
+        self._dataframe[column_name] = pd.to_numeric(self._dataframe[column_name])
+    def _change_column_to_categorical(self, column_name:str) -> None:
+        self._dataframe[column_name] = pd.Categorical(self._dataframe[column_name])
 
     @property
     def dataframe(self) -> pd.DataFrame:
